c68b2e368c8e522a386e33f68d93b539e27274e4
Evgeny <truegraff@mail.ru>
2024-07-15 15:53:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using tasksHW;
+
+string testFilesDir = Path.GetFullPath(@"..\..\..\testFiles");
+
+Console.WriteLine($"Looking for files in {testFilesDir}");
+Console.WriteLine();
+
+var stopwatch = Stopwatch.StartNew();
+int totalSpaces = await TextAnalyser.CountSpacesInDirectoryAsync(testFilesDir);
+stopwatch.Stop();
+
+Console.WriteLine($"Total spaces: {totalSpaces}");
+Console.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds} ms");
---
+++ TextAnalyser.cs
@@ -0,0 +1,21 @@
+﻿namespace tasksHW
+{
+    internal class TextAnalyser
+    {
+        public static async Task<int> CountSpacesInDirectoryAsync(string directoryPath)
+        {
+            var files = Directory.GetFiles(directoryPath);
+            var tasks = files.Select(CountSpacesInFileAsync);
+            var results = await Task.WhenAll(tasks);
+
+            return results.Sum();
+        }
+
+        private static async Task<int> CountSpacesInFileAsync(string filePath)
+        {
+            string content = await File.ReadAllTextAsync(filePath);
+
+            return content.Count(char.IsWhiteSpace);
+        }
+    }
+}
